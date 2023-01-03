0c3f26d9af67408d0f5c8fc3543fd032c18a3d65
davirochabernardes <davirochabernardes@hotmail.com>
2023-01-03 20:36:14 -0300

    solving problem 1003

---
+++ 1003.js
@@ -0,0 +1,10 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+
+var lines = input.split('\r\n');
+
+let num1 = Number(lines.shift());
+let num2 = Number(lines.shift());
+
+let soma = num1 + num2;
+
+console.log(`SOMA = ${soma}`);
